b7e9c88349511df8f7bcdaffa7b9972eb8a1cc48
Liam Jongsu Kim <jskim.cfd@gmail.com>
2014-05-12 07:07:04 +0900

    add angular-deckgrid

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,6 +5,7 @@ angular.module('iconSpaceTesterApp')
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
-      'Karma'
+      'Karma',
+      'akoenig.deckgrid'
     ];
   });
